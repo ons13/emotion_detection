d1a399766343748200cd35b7a67709f16855b943
Ons <63161976+ons13@users.noreply.github.com>
2023-09-19 11:49:48 +0100

    update 2

--- dog_mask_simple.py
+++ dog_mask_simple.py
@@ -2,10 +2,11 @@ import cv2
 import numpy as np
 
 
-def apply_mask(face : n.array , mask : np.array)-> np.array:
+def apply_mask(face : np.array , mask : np.array)-> np.array:
     """add the mask to the provided face , and return the face with mask."""
     mask_h , mask_w , _ = mask.shape
     face_h , face_w , _ = face.shape
+   
     #resize the mask to fit the face
     factor = min(face_h / mask_h , face_w / mask_w)
     new_mask_w = nt(factor * mask_w)
@@ -14,7 +15,7 @@ def apply_mask(face : n.array , mask : np.array)-> np.array:
     #add mask to face ensure is centred 
     resized_mask = cv2.resize(mask , new_mask_shape)
     cv2.imwrite('outputs/resized_dog.png' , resized_mask)
-    
+
 def main():
     face = cv2.imread('assets/me.jpg')
     mask = cv2.imread('assets/dog_mask.png')
